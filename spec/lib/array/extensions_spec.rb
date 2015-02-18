49f514615b7c47e97fdb538bbb4ba31cb6872d09
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-02-18 10:03:38 -0600

    update example description to state json objects explicity

--- spec/lib/array/extensions_spec.rb
+++ spec/lib/array/extensions_spec.rb
@@ -24,13 +24,13 @@ describe Array do
   end
 
   describe '#order' do
-    it 'returns an array sorted by date descending' do
+    it 'returns an array of json objects sorted by date descending' do
       json_objects = EssgeeSoccer::Post.all.order(date: :desc)
       dates        = json_objects.map { |json_object| json_object['date'] }
       expect(dates).to eql dates_desc
     end
 
-    it 'returns an array sorted by date ascending' do
+    it 'returns an array of json objects sorted by date ascending' do
       json_objects = EssgeeSoccer::Post.all.order(date: :asc)
       dates        = json_objects.map { |json_object| json_object['date'] }
       expect(dates).to eql dates_asc
