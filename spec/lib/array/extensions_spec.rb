763d299c1809842c29570c52586752177d659ef2
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-02-18 09:46:49 -0600

    fix typo

--- spec/lib/array/extensions_spec.rb
+++ spec/lib/array/extensions_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Array do
   let(:data_dir)    { File.dirname(__FILE__) + "/../../support/data/**/*.json" }
-  let(:dats_rand)   { [  "28 Feb 2015 1:00:00 PM",
+  let(:dates_rand)  { [  "28 Feb 2015 1:00:00 PM",
                          "18 Apr 2015 08:00:00 AM",
                          "25 Mar 2015 1:00:00 PM",
 
