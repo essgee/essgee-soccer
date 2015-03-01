bb04cb70cd3644751a2bc9d05f8534f7806a765b
Sonny Gonzalez <sg@essgee.io>
2015-03-01 16:47:37 -0600

    add week day to date

--- lib/essgee_helpers/date_time_helpers.rb
+++ lib/essgee_helpers/date_time_helpers.rb
@@ -1,10 +1,22 @@
 module EssgeeHelpers
   def date(post)
-    case post['type']
+    type       = post['type']
+    day        = day(post)
+    week_day   = week_day(post)
+    month      = month(post)
+    time       = time(post)
+    date_class = "class='post-date'"
+    time_class = "class='time'"
+
+    case type
     when 'announcement'
-      "<p><span class='post-date'>#{day(post)} #{month(post)}</span></p>"
+      %Q{
+        <p><span #{date_class}>#{week_day} #{day} #{month}</span></p>
+      }
     else
-      "<p><span class='post-date'>#{day(post)} #{month(post)} <span class='time'>#{time(post)}</span></span></p>"
+      %Q{
+        <p><span #{date_class}>#{week_day} #{day} #{month} <span #{time_class}>#{time}</span></span></p>
+      }
     end
   end
 
@@ -14,6 +26,10 @@ module EssgeeHelpers
 
   def day(post)
     DateTime.parse(post['date']).strftime('%-d')
+  end
+
+  def week_day(post)
+    DateTime.parse(post['date']).strftime('%a')
   end
 
   def time(post)
